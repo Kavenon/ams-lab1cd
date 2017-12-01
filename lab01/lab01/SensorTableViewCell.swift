78a2127dfc9b3fe08b18c074ef063edddaa391ab
Użytkownik Gość <Guest@imac-11.local>
2017-12-01 10:53:42 +0100

    utility

---
+++ lab01/lab01/SensorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SensorTableViewCell.swift
+//  lab01
+//
+//  Created by Użytkownik Gość on 01.12.2017.
+//  Copyright © 2017 Użytkownik Gość. All rights reserved.
+//
+
+import UIKit
+
+class SensorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
